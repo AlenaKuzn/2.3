9d4497672cae08aa3171a108c86b8f385d8b1c04
AlenaKuzn <99790256+AlenaKuzn@users.noreply.github.com>
2022-05-19 21:20:13 +0300

    hard

---
+++ hard/hard.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Задание повышенной сложности
+
+if __name__ == '__main__':
+    slovo = input("Введите слово: ")
+    print("Разных букв: ", len(set(slovo)))
