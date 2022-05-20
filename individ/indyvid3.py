4982fe51b09bd38cbbac6ca5ce0cf45128ff4c2d
AlenaKuzn <99790256+AlenaKuzn@users.noreply.github.com>
2022-05-20 11:28:36 +0300

    ind

--- individ/indyvid3.py
+++ individ/indyvid3.py
@@ -3,3 +3,10 @@
 # Задание 3. Вариант 3
 
 if __name__ == '__main__':
+    predlog = input("Введите предложение: ")
+
+    print("Введите диапазон для удаления: ")
+    n1 = int(input())
+    n2 = int(input())
+
+    print(str(predlog[:n1]), str(predlog[n2:]))
